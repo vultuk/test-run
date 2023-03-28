b36549665e6ebb10f0930e88cf86ac8b109c089c
Simon Skinner <github.ca@vult.uk>
2023-03-28 11:07:52 +0000

    WIP

--- functions/read-pdf.js
+++ functions/read-pdf.js
@@ -46,11 +46,10 @@ exports.handler = async (event, context) => {
     };
   } catch (error) {
     console.error('Error:', error);
-    console.log(error.response.data)
 
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred while processing the PDF file', data: error.response.data }),
+      body: JSON.stringify({ error: 'An error occurred while processing the PDF file', data: error.response.data ?? error }),
     };
   }
 };
