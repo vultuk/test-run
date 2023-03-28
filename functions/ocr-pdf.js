ba63ec09b4b38520cfe4a6a0972f0b1fb223da0e
Simon Skinner <github.ca@vult.uk>
2023-03-28 12:44:03 +0000

    Trying to shrink

--- functions/ocr-pdf.js
+++ functions/ocr-pdf.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 const pdfjsLib = require('pdfjs-dist/es5/build/pdf');
 const Tesseract = require('tesseract.js');
 const path = require('path');
@@ -6,27 +6,27 @@ const path = require('path');
 pdfjsLib.GlobalWorkerOptions.workerSrc = path.join(__dirname, './node_modules/pdfjs-dist/es5/build/pdf.worker.js');
 
 async function readPdfText(pdfUrl) {
-    const response = await fetch(pdfUrl);
-    const data = new Uint8Array(await response.arrayBuffer());
-    const pdfDocument = await pdfjsLib.getDocument({ data }).promise;
-    const totalPages = pdfDocument.numPages;
-    let fullText = '';
-  
-    for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
-      const page = await pdfDocument.getPage(pageNumber);
-      const viewport = page.getViewport({ scale: 1 });
-      const canvas = Tesseract.createCanvas(viewport.width, viewport.height);
-      const context = canvas.getContext('2d');
-      const renderContext = {
-        canvasContext: context,
-        viewport: viewport,
-      };
-      await page.render(renderContext).promise;
-      const result = await Tesseract.recognize(canvas);
-      fullText += result.data.text;
-    }
-  
-    return fullText;
+  const response = await axios.get(pdfUrl, { responseType: 'arraybuffer' });
+  const data = new Uint8Array(response.data);
+  const pdfDocument = await pdfjsLib.getDocument({ data }).promise;
+  const totalPages = pdfDocument.numPages;
+  let fullText = '';
+
+  for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+    const page = await pdfDocument.getPage(pageNumber);
+    const viewport = page.getViewport({ scale: 1 });
+    const canvas = Tesseract.createCanvas(viewport.width, viewport.height);
+    const context = canvas.getContext('2d');
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    await page.render(renderContext).promise;
+    const result = await Tesseract.recognize(canvas);
+    fullText += result.data.text;
+  }
+
+  return fullText;
   }
 
   exports.handler = async (event, context) => {
