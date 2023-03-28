6451299ec527ea692e8b25d4b8f62562d3491be8
Simon Skinner <github.ca@vult.uk>
2023-03-28 11:42:12 +0000

    Parse Text

---
+++ functions/ocr-pdf.js
@@ -0,0 +1,47 @@
+const fetch = require('node-fetch');
+const pdfjsLib = require('pdfjs-dist/es5/build/pdf');
+const Tesseract = require('tesseract.js');
+const path = require('path');
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = path.join(__dirname, './node_modules/pdfjs-dist/es5/build/pdf.worker.js');
+
+async function readPdfText(pdfUrl) {
+    const response = await fetch(pdfUrl);
+    const data = new Uint8Array(await response.arrayBuffer());
+    const pdfDocument = await pdfjsLib.getDocument({ data }).promise;
+    const totalPages = pdfDocument.numPages;
+    let fullText = '';
+  
+    for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+      const page = await pdfDocument.getPage(pageNumber);
+      const viewport = page.getViewport({ scale: 1 });
+      const canvas = Tesseract.createCanvas(viewport.width, viewport.height);
+      const context = canvas.getContext('2d');
+      const renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+      };
+      await page.render(renderContext).promise;
+      const result = await Tesseract.recognize(canvas);
+      fullText += result.data.text;
+    }
+  
+    return fullText;
+  }
+
+  exports.handler = async (event, context) => {
+    const url = event.queryStringParameters.url;
+    if (!url) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Missing PDF URL in the request' }),
+      };
+    }
+
+    const text = await readPdfText(url);
+
+    return {
+        statusCode: 500,
+        body: JSON.stringify(text),
+      };
+  }
